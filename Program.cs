71d61a75c1a19acbe56e784488cb9e2f4f9154fd
Nurettin <107817157+Kelxasi@users.noreply.github.com>
2022-12-08 23:12:21 +0300

    print messasges in green

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ 
+ Console.ForegroundColor = ConsoleColor.Green;
+ Console.WriteLine("-> Ben yeşilim ");
+ Console.ResetColor();
